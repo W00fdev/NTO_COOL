6f4fc240dfb1baa50a5d27615776e7b630155cad
Dmitry Kulikov <w00f.devs@gmail.com>
2024-12-06 15:43:38 +0300

    feat: bear worker animation

--- Assets/Scripts/AI/BearAgent.cs
+++ Assets/Scripts/AI/BearAgent.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class BearAgent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _resourceItem;
+    
+    [SerializeField] private Vector3 _startPosition;
+    [SerializeField] private Vector3 _targetPosition;
+    [SerializeField] private float _speed;
+
+    [SerializeField] private Animator _animator;
+    
+    private bool _toWork;
+    private bool _prevToWork;
+    
+    private void Start()
     {
+        _startPosition = transform.position;
+        _toWork = true;
+        _prevToWork = false;
+    }
+
+    private void Update()
+    {
+        Vector3 direction = Vector3.zero;
+        
+        if (_toWork)
+        {
+            if (_prevToWork == false)
+                ToWork();
+
+            direction = (_targetPosition - _startPosition).normalized;
+
+            if (Vector3.Distance(transform.position, _targetPosition) < 0.5f)
+                _toWork = false;
+        }
+        else
+        {
+            if (_prevToWork)
+                FromWork();
+
+            direction = (_startPosition - _targetPosition).normalized;
+            
+            if (Vector3.Distance(transform.position, _startPosition) < 0.5f)
+                _toWork = true;
+        }        
+        
+        transform.position += direction * (_speed * Time.deltaTime);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ToWork()
     {
+        _toWork = true;
+        _prevToWork = true;
         
+        _animator.SetTrigger("ToWork");
+        
+        _resourceItem.gameObject.SetActive(false);
+        transform.forward = Vector3.forward;
+    }
+    
+    private void FromWork()
+    {
+        _toWork = false;
+        _prevToWork = false;
+        
+        _animator.SetTrigger("FromWork");
+        _resourceItem.gameObject.SetActive(true);
+        transform.forward = Vector3.back;
     }
 }
